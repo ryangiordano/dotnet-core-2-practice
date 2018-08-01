2d47f341b43114b0079af821877b13ba2bca813f
thedaruma <rgiorda1@gmail.com>
2018-07-31 20:06:34 -0400

    Add automapper

---
+++ DatingApp.API/Models/Photo.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace DatingApp.API.Models
+{
+  public class Photo
+  {
+    public int Id { get; set; }
+    public string Url { get; set; }
+    public string Description { get; set; }
+    public DateTime DateAdded { get; set; }
+    public bool IsMain { get; set; }
+    //Now that we define the relationship both ways, we can perform a cascade delete.
+    public User User { get; set; }  
+    public int UserId { get; set; }
+  }
+}
